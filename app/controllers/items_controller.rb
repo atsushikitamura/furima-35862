55e844aea54adecd12b1afd851d09000c13e21b8
atsushikitamura <ax2kx8@gmail.com>
2021-06-20 22:23:48 +0900

    出品した商品の保存機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,24 @@ class ItemsController < ApplicationController
 
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :text, :category_id, :item_status_id, :delivery_fee_id, :prefecture_id, :delivery_period_id, :price).merge(user_id: current_user.id)
+  end
+
 end
