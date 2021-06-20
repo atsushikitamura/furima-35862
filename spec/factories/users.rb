b20a6bd936521e09d6b15908186a11a3b65db16b
atsushikitamura <ax2kx8@gmail.com>
2021-06-20 14:18:12 +0900

    単体テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
-    password_confirmation { password } # この行までカリキュラムコピペ
+    password              { '99999a' }
+    password_confirmation { password }
     family_name           { '石井' }
     first_name            { '拓哉' }
     family_kana           { 'イシイ' }
