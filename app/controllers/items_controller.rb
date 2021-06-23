73ee474e3159a7d4778c393f9fcdf4c61f0e8c1a
atsushikitamura <ax2kx8@gmail.com>
2021-06-23 18:28:38 +0900

    商品削除機能　訂正#2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :destroy]
+  before_action :authenticate_user!, only: [:new, :create, :destroy]
   before_action :set_item, only: [:show, :destroy]
   before_action :move_to_index, only: :destroy
 
